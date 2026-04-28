b4f96c2806a553e6bcb1aa1682552cc4c7e5de2d
onemest9-lgtm <onemest9@gmail.com>
2026-04-28 16:06:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Mesty
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            panel1 = new Panel();
+            button4 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button1 = new Button();
+            pnlTabs = new Panel();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Dock = DockStyle.Fill;
+            webView21.Location = new Point(0, 41);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(1136, 458);
+            webView21.TabIndex = 1;
+            webView21.ZoomFactor = 1D;
+            webView21.Click += webView21_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1136, 41);
+            panel1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(883, 7);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 0;
+            button4.Text = "Главное";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(845, 7);
+            button2.Name = "button2";
+            button2.Size = new Size(32, 29);
+            button2.TabIndex = 6;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(101, 7);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(738, 27);
+            textBox1.TabIndex = 5;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(50, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(32, 29);
+            button3.TabIndex = 4;
+            button3.Text = ">";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(12, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 29);
+            button1.TabIndex = 2;
+            button1.Text = "<";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pnlTabs
+            // 
+            pnlTabs.BackColor = Color.White;
+            pnlTabs.Dock = DockStyle.Bottom;
+            pnlTabs.Location = new Point(0, 499);
+            pnlTabs.Name = "pnlTabs";
+            pnlTabs.Size = new Size(1136, 33);
+            pnlTabs.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1136, 532);
+            Controls.Add(webView21);
+            Controls.Add(pnlTabs);
+            Controls.Add(panel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Mesty";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private Panel panel1;
+        private Button button3;
+        private Button button1;
+        private TextBox textBox1;
+        private Panel pnlTabs;
+        private Button button2;
+        private Button button4;
+    }
+}
